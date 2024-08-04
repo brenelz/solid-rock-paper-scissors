f49ce93fe7c56ab8306c0b484b7cde908b57335e
Brenley Dueck <brenleydueck@gmail.com>
2024-08-03 21:39:01 -0500

    Initial template

---
+++ src/components/RockPaperScissorsGame.tsx
@@ -0,0 +1,5 @@
+export default function RockPaperScissorsGame() {
+    return <>
+        Game goes here
+    </>
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,10 @@
+import RockPaperScissorsGame from "~/components/RockPaperScissorsGame";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Rock, Paper, Scissors</h1>
+      <RockPaperScissorsGame />
+    </div>
+  );
+}
